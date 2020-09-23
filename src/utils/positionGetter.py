44fb47b016ca22cd99cfb0aaf2ee0780e7894fd5
Steve <peviroy@outlook.com>
2020-09-23 10:40:03 +0800

    feat(positiongetter): purify code, more appropriote

--- src/utils/positionGetter.py
+++ src/utils/positionGetter.py
@@ -1,57 +1,36 @@
+"""
+@Author: peviroy
+@Last Modified by: peviroy
+@Last Modified time: 2020-09-23
+"""
 import pyautogui
+
 from utils.imageGrab import grab_screen
-import pytesseract
-from PIL import Image
-import os
-
-
-def numberCapture(name, min_limit, pos1_x, pos1_y, pos2_x, pos2_y):
-    # if not captured, retry for 5 times to make sure: 1.screenshot is right; number is recgonized correctly
-    for count in range(5):
-        img = grab_screen(pos1_x, pos1_y, pos2_x, pos2_y)
-        img.save(os.path.join('../../screenshots', name + '.png'))
-        img = Image.open(os.path.join('../../screenshots', name + '.png'))
-
-        number = pytesseract.image_to_string(
-            img, lang="chi_sim", config='--psm 7 --oem 0 -c tessedit_char_whitelist=0123456789')
-
-        try:
-            number = int(number)
-            print(number)
-            # if name == 'HP' and number == 31:   # 单个体择优
-            # return 6
-            if number == min_limit or number == min_limit + 1:
-                return 1
-            return 0
-        except Exception:
-            continue
-
-    print('not captured, skip this circle')
-    return -1
 
 
 class PositionGetter():
-    '''Place your mouse at the position where you want to konw, and keydown ENTER for alert box
-    Usage: PositionGetter(['HO', 'HI', 'HP']).run()
+    '''
+    Place your mouse at the position where you want to konw, and keydown ENTER for alert box
+    Usage: posGetter = PositionGetter(mode='AREA')
+           posGetter(['HP', 'WF'])
     '''
 
-    def __init__(self, position_names, mode='BUTTON'):
+    def __init__(self, mode: str):
         '''
-        :param [position_names]--list
+        :param [mode]--str mode-POINT, for each pos, one point and without repeat confirmation;
+                           mode-AREA, for each pos, two point and with repeat confirmation;
         '''
-        self.position_names = position_names
         self.mode = mode
 
-    def run(self):
+    def __call__(self, position_names: list):
         '''
-        :param [mode]--str mode-BUTTON, for each pos, one point and without confirm;
-                           mode-AREA, for each pos, two point and with confirmation;
+        :param [position_names]-- str list, 1-D
         '''
         positions = []  # record | return
-        for position_name in self.position_names:
-            if self.mode == 'BUTTON' or self.mode == 'FLASH':
+        for position_name in position_names:
+            if self.mode == 'POINT':
                 pyautogui.alert(
-                    'Place mouse for "{0}", and key down ENTER'.format(position_name))
+                    'Place mouse for "{0}", and key down ENTER to confirm choice'.format(position_name))
                 currentMouseX, currentMouseY = pyautogui.position()
                 print('{0} lies at ({1}, {2})'.format(
                     position_name, currentMouseX, currentMouseY))
@@ -63,14 +42,12 @@ class PositionGetter():
                     point_area = []  # area consists of two point
                     for index in range(2):
                         pyautogui.alert(
-                            'Place mouse for "{0}-{1}", and key down ENTER'.format(position_name, index + 1))
+                            'Place mouse for "{0}-pos{1}", and key down ENTER to confirm choice'.format(position_name, index + 1))
                         currentMouseX, currentMouseY = pyautogui.position()
-                        print('{0}-{3} lies at ({1}, {2})'.format(
+                        print('{0}-pos{3} lies at ({1}, {2})'.format(
                             position_name, currentMouseX, currentMouseY, index + 1))
                         point_area.append((currentMouseX, currentMouseY))
-                    if self._showImageArea(point_area):
-                        numberCapture('TT', 0, point_area[0][0], point_area[0][1],
-                                      point_area[1][0], point_area[1][1])
+                    if self.areaCapture(point_area):
                         if pyautogui.confirm('Is this proper?') == 'OK':
                             break
                 positions.append((position_name, point_area))
@@ -79,22 +56,25 @@ class PositionGetter():
         print('Summary:', dict(positions))
         return dict(positions)
 
-    def _showImageArea(self, point_area):
+    def areaCapture(self, point_area: list) -> bool:
         '''
-        :param [point_area]-list | example: [(1, 1), (2, 2)]
+        Note:
+        -----------
+            Capture a digital image from designated area
+        ARGS:
+        -----------
+            name: {str} -- the name of image to be identified for saving img locally
+            point_area {list} -- [[x1, y1], [x2, y2]]
         '''
-        x1, y1, x2, y2 = [point_area[0][0], point_area[0][1],
-                          point_area[1][0], point_area[1][1]]
-        if x1 > x2 or y1 > y2:
-            print('Area selection error: second point must be right below')
+        pos1_x, pos1_y, pos2_x, pos2_y = [
+            coord for point in point_area for coord in point]
+        if pos1_x > pos2_x or pos1_y > pos2_y:
+            pyautogui.alert(
+                'Area selection error: second point must be right below')
             return False
-        img = grab_screen(x1, y1, x2, y2)
+        # retry for 2 times to make sure: target area is captured
+        for count in range(2):
+            img = grab_screen(pos1_x, pos1_y, pos2_x, pos2_y)
+
         img.show()
         return True
-
-
-if __name__ == "__main__":
-    print("|---------------Testing for BUTTON mode'----------------|")
-    PositionGetter(['HO', 'HI', 'HP']).run()
-    print("|---------------Testing for Area mode'------------------|")
-    PositionGetter(['HO', 'HI', 'HP'], mode='AREA').run()
